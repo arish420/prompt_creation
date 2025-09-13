ae90dc42b8ba8562df6b66df1d8c140307afc96f
arish420 <muhammadarish420@gmail.com>
2025-09-13 22:34:02 +0500

    Update prompt.py

--- prompt.py
+++ prompt.py
@@ -17,7 +17,7 @@ if selection == 'Personal PII':
   personal_pii_options = st.multiselect(
     "What are your favorite colors?",
     ["Name", "Email", "Phone No", "Address", "Date of Birth", "Mother's name","Social Media Handles", "National ID Number (e.g., CNIC, SSN, Aadhaar)","Passport Number","Driver’s License Number","Voter ID Number","Tax Identification Number","Bank Account Number","Credit/Debit Card Numbers","Digital Wallet IDs (PayPal, Apple Pay, etc.)"],
-    default=["Name", "Email", "Phone"])
+    default=["Name", "Email", "Phone No"])
   st.write(personal_pii_options)
 
   with st.expander("Regular Expression/Few-shots"):
