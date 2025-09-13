772c87ce1073c0078dbf073a8c02aff7c985ebc1
arish420 <muhammadarish420@gmail.com>
2025-09-13 19:25:48 +0500

    Update prompt.py

--- prompt.py
+++ prompt.py
@@ -3,4 +3,16 @@ import streamlit as st
 
 st.title("Create your Custom Prompts")
 
-selection = st.sidebar.selectbox('Select', ('Personal PII','Medical PII','Financial PII'))
+selection = st.sidebar.selectbox('Select', ('Home','Personal PII','Medical PII','Financial PII'))
+
+if selection == 'Home':
+  st.write("Welcome to Data Privacy using LLMs")
+
+if selection == 'Personal PII':
+  country = st.selectbox('Select', ('Pakistan','USA'))
+  options = st.multiselect(
+    "What are your favorite colors?",
+    ["Green", "Yellow", "Red", "Blue"],
+    default=["Yellow", "Red"],
+    )
+
