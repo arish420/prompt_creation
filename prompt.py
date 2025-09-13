23f969038f7d3ce9dbd2ec713dbeea5abd6972dd
arish420 <muhammadarish420@gmail.com>
2025-09-13 22:17:46 +0500

    Update prompt.py

--- prompt.py
+++ prompt.py
@@ -68,7 +68,7 @@ if selection == 'Personal PII':
     st.write(personal_pii)
     data = personal_pii.save("template.json")
     # Convert dict to JSON string
-    json_str = json.dumps(personal_pii.save("template.json"), indent=4)
+    json_str = json.dumps(personal_pii.dict(), indent=4)
     st.write(data)
     
     # Encode as base64 to make a download link
